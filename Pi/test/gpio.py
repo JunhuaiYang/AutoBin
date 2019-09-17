5c17ccf34e6117798d2b247fcb12e515f53b9adb
Junhuai <850217160@QQ.com>
2019-09-17 19:03:30 +0800

    test

---
+++ Pi/test/gpio.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import sys
+import time
+LED = 4
+def main():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LED,GPIO.OUT)
+    while (True):
+        GPIO.output(LED,True)
+        time.sleep(0.5)
+        GPIO.output(LED,False)        
+        time.sleep(0.5)
+main()
